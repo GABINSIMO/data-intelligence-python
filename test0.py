24ee91e0587764f4771bdbff7d9d9e98778cb3ac
GABINSIMO <108668446+GABINSIMO@users.noreply.github.com>
2022-07-04 13:00:57 +0200

    Add files via upload

---
+++ test0.py
@@ -0,0 +1,30 @@
+
+valore = input ("inserire un valore")
+if (valore !=0 ):
+ print (valore)
+
+
+lista = []
+valore = input ("inserire il primo valore")
+lista.append (valore)
+valore = input ("inserire il secondo valore")
+lista.append (valore)
+valore = input ("inserire il terzo valore")
+lista.append (valore)
+if (lista !=0):
+ print (lista)
+
+
+lista = []
+valore = input ("inserire il primo valore")
+lista.append (valore)   
+valore = input ("inserire il secondo valore")
+lista.append (valore)
+valore = input ("inserire il terzo valore")
+lista.append (valore)
+if (lista != 0):
+ print (lista)
+
+
+
+
