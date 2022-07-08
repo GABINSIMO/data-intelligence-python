b3c7b24950f183c9ba92d5720f3887ef732b8c84
GABINSIMO <108668446+GABINSIMO@users.noreply.github.com>
2022-07-08 17:44:41 +0200

    Add files via upload

---
+++ classe_studenti.py
@@ -0,0 +1,28 @@
+#classe per studenti
+
+class person():
+    def _init_(self, fname, lname):
+        self.firstname = fname
+        self.lastname = lname
+
+
+
+class docente(person):
+    #se voglio agguingere un altro campo per studente posso fare una altro init
+    def _init_(self, fname, lname, year):
+        super._init_(self, fname, lname)
+        self.my_year=year
+
+    def printname (self):
+        print (self.firstname, self.lastname, self.my_year)
+   
+
+class studente(docente):
+    def _int_(self, fname, lname, year, media):
+        super._init_(self,fname, lname, year)
+        self.average= media
+
+    def printname (self):
+        print (self.firstname, self.lastname, self.my_year, self.average)
+    
+  
